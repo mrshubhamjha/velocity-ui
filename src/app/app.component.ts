06c09900228a0bf01ad004678df21367028b8ecd
mrshubhamjha <shubhamjha314@gmail.com>
2025-07-18 01:06:02 +0530

    header added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'velocity-ui';
+  isDarkMode = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      const themeAttr = document.documentElement.getAttribute('data-theme');
+      if (themeAttr === 'dark' || (!themeAttr && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        this.isDarkMode = true;
+        document.documentElement.setAttribute('data-theme', 'dark');
+      } else {
+        this.isDarkMode = false;
+        document.documentElement.setAttribute('data-theme', 'light');
+      }
+    }
+  }
+
+  toggleTheme() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDarkMode = !this.isDarkMode;
+      document.documentElement.setAttribute('data-theme', this.isDarkMode ? 'dark' : 'light');
+    }
+  }
 }
